e7f6dfde6a2eb4cabcaf914237f28e6f1c9165ef
Nordy vlasman <nordyvla@gmail.com>
2020-09-25 21:02:30 +0200

    wip

--- Sources/DeclarativeUI/Models/List.swift
+++ Sources/DeclarativeUI/Models/List.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
-public struct ListType: Decodable {
+public struct ListType: Decodable, HasAction {
     let title: String
+    var action: Action? = nil
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ActionCodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        action = try decodeAction(from: container)
+    }
 }
