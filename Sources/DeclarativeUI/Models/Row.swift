daff6119b3d0cd8c818e686ab83f4d578f5a889d
Nordy vlasman <nordyvla@gmail.com>
2020-09-25 20:55:53 +0200

    wip

--- Sources/DeclarativeUI/Models/Row.swift
+++ Sources/DeclarativeUI/Models/Row.swift
@@ -17,7 +17,7 @@ public struct Row: Decodable, HasAction {
         let container = try decoder.container(keyedBy: ActionCodingKeys.self)
         title = try container.decode(String.self, forKey: .title)
         type = try container.decodeIfPresent(String.self, forKey: .type)
-        listRows = try container.decode([ListType]?.self, forKey: .list)
+        listRows = try container.decodeIfPresent([ListType].self, forKey: .list)
         action = try decodeAction(from: container)
     }
 }
