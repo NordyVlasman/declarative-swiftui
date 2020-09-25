2390f29904e3dc06bb655d1a8de8f881a741b2ab
Nordy vlasman <nordyvla@gmail.com>
2020-09-25 19:54:43 +0200

    wip

--- Sources/DeclarativeUI/Nodes/NavigationComponent.swift
+++ Sources/DeclarativeUI/Nodes/NavigationComponent.swift
@@ -35,6 +35,12 @@ struct NavigationViewComponent: View, NavigationParams, UIDelegate {
     var title: String
     var rows: [Row]
     
+    init(title: String, rows: [Row]) {
+        print("Test")
+        self.title = title
+        self.rows = rows
+    }
+    
     var body: some View {
         NavigationView {
             renderPage(ui: controller.uiComponents, uiDelegate: self)
